7b3f8761861cd5d8ec96ab47438b7640d4db6d92
João Victor Corado <jhonisilva545@gmail.com>
2023-05-17 18:13:42 -0300

    jv

--- components/Agendar/index.tsx
+++ components/Agendar/index.tsx
@@ -4,7 +4,7 @@ export const Agendar = () => {
   return (
     <div>
         <button className='bg-primario p-4 rounded-xl text-xl'>
-            Agendar consulta agora
+            Agendar consulta
         </button>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Section1 } from '@/components/Section1'
-import Image from 'next/image'
-
+import { Section2 } from '@/components/Section2'
+import { Section3 } from '@/components/Section3'
 
 export default function Home() {
   return (
    <div className="">
       <Section1/>
+      <Section2/>
+      <Section3/>
    </div>
   )
 }
