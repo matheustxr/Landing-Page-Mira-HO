4e9e585c0dbd129df3dbaf82a8561c6eb7bcdbf2
João Victor Corado <jhonisilva545@gmail.com>
2023-05-17 16:29:41 -0300

    jv

---
+++ components/Section1/index.tsx
@@ -0,0 +1,8 @@
+export const Section1 = () => {
+  return (
+    <div className="bg-section-1 bg-[#2D509E] flex flex-col items-center justify-center text-white h-screen">
+        <h1>Entre em contato conosco</h1>
+        <p>Cuide da Saúde dos seus Olhos, tire suas dúvidas e agende uma consulta agora.﻿</p>
+    </div>
+  )
+}
