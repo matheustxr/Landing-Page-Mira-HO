99bcc40da7ba2010800246d87013f3e63b2d985b
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-05-19 12:00:50 -0300

    mt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { Section1 } from '@/components/Section1'
 import { Section2 } from '@/components/Section2'
 import { Section3 } from '@/components/Section3'
-import Contato from "@/components/Contato"
 import Section4  from '@/components/Section4'
+import Section5  from '@/components/Section5'
 import { Footer } from '@/components/Footer'
+import Contato from "@/components/Contato"
 
 
 
@@ -16,8 +17,8 @@ export default function Home() {
         <Section3/>
         <Contato/>
       </div>
-      
       <Section4/>
+      <Section5 />
       <Footer/>
    </div>
   )
